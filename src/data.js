3b25003a15910a33552fa2fbed8c4ce57240abe0
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2021-09-01 16:12:44 +0200

    create(): react frontend

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirts",
+      category: "Shirts",
+      image: "./../images/product-1.jpg",
+      price: "$120",
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 90,
+      description: "High Quality",
+    },
+    {
+      _id: "2",
+      name: "Italian Slim Shirts",
+      category: "Shirts",
+      image: "./../images/product-2.jpg",
+      price: "$140",
+      brand: "Lacoste",
+      rating: 4.5,
+      numReviews: 1,
+      description: "Original",
+    },
+    {
+      _id: "3",
+      name: "Chinese Slim Shirts",
+      category: "Shirts",
+      image: "./../images/product-3.jpg",
+      price: "$20",
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 100,
+      description: "Chinise",
+    },
+  ],
+};
+
+export default data;
